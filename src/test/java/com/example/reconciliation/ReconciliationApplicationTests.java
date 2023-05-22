662fc2e2ea7ecd4312856f5c71cc44728da2c5ce
supinglan <s2530555829@outlook.com>
2023-05-22 10:51:18 +0800

    init

---
+++ src/test/java/com/example/reconciliation/ReconciliationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reconciliation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReconciliationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
